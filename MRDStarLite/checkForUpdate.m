0e3776cc61c5f79a09924a5569acb0b852e2e44f
MortezaHagh <m.haghbeigi@gmail.com>
2023-07-08 01:08:42 +0200

    rm

--- MRDStarLite/checkForUpdate.m
+++
@@ -1,6 +0,0 @@
-function [Open, Robot] = checkForUpdate(Open, GV, Robot, nodesForUpdate, Model)
-
-    % update vertex
-    [Open, Robot] = updateVertex(Open, GV, Robot, nodesForUpdate, Model);
-
-end
